b8cfb12e1f57ef7f4983efa37443f2a41ccc3047
SooHoon Choi <soohoonchoi@gmail.com>
2024-01-20 23:54:10 -0800

    initial commit non working postgres

---
+++ src/server/db/index.ts
@@ -0,0 +1,16 @@
+import { Client } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import { env } from "@/env";
+import * as schema from "./schema";
+
+const client = new Client({
+  connectionString: env.DATABASE_URL,
+});
+
+await client.connect();
+
+export const db = drizzle(
+  client,
+  { schema }
+);

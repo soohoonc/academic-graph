ab8ea15af22cc845439e59bee7f7154928a3610a
SooHoon Choi <soohoonchoi@gmail.com>
2024-02-19 23:29:38 -0800

    turbo

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
+import Graph from "@/components/graph";
+
+export default async function Home() {
+  noStore();
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="flex min-h-screen">
+      <Header />
+      <Sidebar />
+      <Graph />
+    </main>
+  );
+}
---
+++ apps/web/src/components/graph.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Graph = () => {
+  return (
+    <div>Graph</div>
+  )
+}
+
+export default Graph

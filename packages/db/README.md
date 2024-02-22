d1c0caec2723b0ed00241bcf8583d181f0f76f64
SooHoon Choi <soohoonchoi@gmail.com>
2024-02-21 22:29:10 -0800

    refactoring for monorepo

---
+++ packages/db/README.md
@@ -0,0 +1,1 @@
+# Database files for academic graph
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,11 +1,9 @@
 import { type Config } from "drizzle-kit";
 
-import { env } from "@/env";
-
 export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config;
